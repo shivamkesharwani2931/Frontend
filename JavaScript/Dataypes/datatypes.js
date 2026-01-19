426860884e12bb6a41e21dbcd838577054bef3e4
shivamkesharwani2931 <shivamkesharwani2910@gmail.com>
2026-01-19 13:11:49 +0530

    datatypes

---
+++ JavaScript/Dataypes/datatypes.js
@@ -0,0 +1,18 @@
+var xyz = "Shivam";
+var xyz= "SK";  //can be redeclared
+
+let name = "Shivam";
+// let name = "SK"  //cannot be redeclared
+name = "Shivam k"
+console.log(xyz , name)
+
+const PI=3.141;
+PI = "hiii"; //cannot be redefined and redeclared
+
+var name1 = "String"; //string
+var number1 = 90;  //number
+var bool = false;  //boolean
+var sym= Symbol()  //symbol
+var undef = undefined;  //undefined
+var null1 = null;  //null
+
