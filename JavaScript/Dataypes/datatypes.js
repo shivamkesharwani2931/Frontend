7ddbdd81c7ba3c6b411ff1441e6abd3f208130c3
shivamkesharwani2931 <shivamkesharwani2910@gmail.com>
2026-01-20 13:08:14 +0530

    js datatypes

--- JavaScript/Dataypes/datatypes.js
+++ JavaScript/Dataypes/datatypes.js
@@ -7,12 +7,31 @@ name = "Shivam k"
 console.log(xyz , name)
 
 const PI=3.141;
-PI = "hiii"; //cannot be redefined and redeclared
+//PI = "hiii"; //cannot be redefined and redeclared
 
-var name1 = "String"; //string
-var number1 = 90;  //number
-var bool = false;  //boolean
-var sym= Symbol()  //symbol
-var undef = undefined;  //undefined
-var null1 = null;  //null
+// PRIMITIVE DATATYPE
+let name1 = "String"; //string
+let number1 = 90;  //number
+let bool = false;  //boolean
+let sym= Symbol()  //symbol
+let undef = undefined;  //undefined
+let null1 = null;  //null
+let bigInt = 21345472456545;
 
+// NON_PRIMITIVE DATATYPE
+let arr = [
+    "Shivam",
+    "Suraj",
+    2,
+    3,
+    [true,false],
+    1232456355867,
+    {
+        name:"Shivam",
+        age:19
+    }
+];
+
+console.log(arr)
+
+let obj = []
